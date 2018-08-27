2f5adfe88dc374749406bab5fbec4ec91b1b2144
Mark Johnston <Mark.Johnston@sscgp.com>
2018-08-27 16:03:11 -0500

    Added MS test for Create New Order Logs exception (the first of many).

--- AutoMockContext.MSTest/OrderProcessorTests.cs
+++ AutoMockContext.MSTest/OrderProcessorTests.cs
@@ -1,8 +1,11 @@
 ﻿namespace AutoMockHelper.MSTest
 {
+	using System;
+	using System.Collections.Generic;
 	using AutoMockHelper.Core;
 	using AutoMockHelper.SampleLogic.OrderProcessor;
 	using Microsoft.VisualStudio.TestTools.UnitTesting;
+	using Moq;
 
 	[TestClass]
 	public class OrderProcessorTests : AutoMockContext<OrderProcessor>
@@ -22,6 +25,23 @@
 
 			//Assert
 			Assert.IsInstanceOfType(instance, typeof(OrderProcessor));
+		}
+
+		[TestMethod]
+		public void CreateNewOrderLogsException()
+		{
+			//Arrange
+			this.MockFor<ILogger>().Setup(x => x.Info(It.Is<string>(m => m.Contains($"{nameof(OrderProcessor.CreateNewOrder)}"))));
+			this.MockFor<ILogger>().Setup(x => x.Error(It.Is<string>(m => m.Contains($"{nameof(OrderProcessor.CreateNewOrder)}")), It.IsAny<Exception>()));
+
+			this.MockFor<IOrderRepository>().Setup(x => x.SaveNewOrder(It.IsAny<List<OrderItem>>(), It.IsAny<Customer>()))
+											.Throws(new ApplicationException("Order Repository is broken!"));
+
+			//Act
+			this.ClassUnderTest.CreateNewOrder(It.IsAny<List<OrderItem>>(), It.IsAny<Customer>());
+
+			//Assert
+			this.VerifyAll();
 		}
 	}
 }
